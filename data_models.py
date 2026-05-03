8950cc4a6163d81391c654b83ac5ab3bb9f14502
Sripravallika1 <117320390+Sripravallika1@users.noreply.github.com>
2026-05-03 16:44:48 -0500

    Implement LearnerInteraction and LearnerProfile classes

---
+++ data_models.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class LearnerInteraction:
+    def __init__(self, interaction_data):
+        self.interaction_data = interaction_data
+
+class LearnerProfile:
+    def __init__(self, profile_data):
+        self.profile_data = profile_data
